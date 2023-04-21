cc654018b8a7b6dd8f8edaf0caa9bd8b074cf183
Makhosi <thuthukamakhosi23@gmail.com>
2023-04-21 22:16:47 +0200

    updated views file

--- snetechWebsite/snetechmedia/views.py
+++ snetechWebsite/snetechmedia/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
+    """This method will be used to direct the user to the home page
+
+        :returns: Home page
+
+        :rtype: html
+    """
     new = News.objects.all()
     return render(request, "home.html", {'new': new} )
 
